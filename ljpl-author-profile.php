bb2db8de91133dc9c43cfcd10b4ffb726aae756d
ljasinskipl <ljasinskipl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-19 15:26:19 +0000

    Initial import.
    git-svn-id: https://plugins.svn.wordpress.org/gplus-author-profile/trunk@465097 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ljpl-author-profile.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ Plugin Name: GPlus Link Author Profile
+ Plugin URI: http://blog.ljasinski.pl
+ Description: Simple plugin to get your author's profile linked in Google. 
+ Grants autorship photo in Google SERP. Visit blog.ljasinski.pl for more details.
+ All settings are per user, change them in your profile.
+ Version: 1.0
+ Author: Łukasz Jasiński
+ Author URI: http://www.ljasinski.pl
+ Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
+ It does *some* sanitation but beware who you allow to write bio in you site, because
+ now default wordpress filter for bio is off!
+ */
+
+// #############################
+// ### Add rel-author to the end of article
+
+function jlpl_gplus_content_link($text) {
+	$text .= 
+		'<p class="author-link">' .
+		get_the_author_meta('ljplcontentauthortext') .
+		' <a href="' . 
+		get_author_posts_url(get_the_author_meta( 'ID' )).
+		'" rel="author">' .
+		get_the_author_meta('display_name') . 
+		'</a></p>';
+	
+	return $text;
+}
+
+
+	add_filter('the_content', 'jlpl_gplus_content_link');	
+// #############################
+// ### Adding link to author's profile 	
+	
+function jlpl_gplus_bio_link($text) {
+	
+	$find = '[gplus]';
+	if(get_the_author_meta('gplus')) {
+		$gplus = the_author_meta('gplus');		
+		$replace = "
+			<a rel=\"me\" href=\"".$gplus."\" title=\"My Google+ profile\">
+			  <img src=\"http://www.google.com/images/icons/ui/gprofile_button-32.png\" width=\"32\" height=\"32\">
+			</a>";
+	} else {
+		$replace = "";
+	}
+	$text = str_replace($find,$replace,$text);
+	$text = strip_tags($text,'<a><img>');
+	return $text;
+}
+
+// #############################
+// ### Backend settings
+	
+add_action( 'show_user_profile', 'ljpl_gplus_link_field_add' );
+add_action( 'edit_user_profile', 'ljpl_gplus_link_field_add' );
+
+remove_filter('pre_user_description', 'wp_filter_kses');
+add_filter('pre_user_description', 'jlpl_gplus_bio_link');	
+
+function ljpl_gplus_link_field_add( $user ) { ?>
+
+	<h3>GPlus Link Author Profile</h3>
+	<table class="form-table">
+		<tr>
+			<th><label for="gplus">Google+</label></th>
+			<td>
+				<input type="text" name="gplus" id="gplus" value="<?php echo esc_attr( get_the_author_meta( 'gplus', $user->ID ) ); ?>" class="regular-text" /><br />
+				<span class="description">Paste your Google+ profile URL. After saving 
+				insert [gplus] code into your bio to add a tagged link to your profile.
+				</span>
+			</td>
+		</tr>
+		<tr><th><label for="ljpl-content-author-show"></label></th>
+			<td>
+				<input type="checkbox" name="ljpl-content-author-show" id="ljpl-content-author-show"<?php if(get_the_author_meta( 'ljplcontentauthorshow', $user->ID ) ) echo ' checked="checked"' ?>" class="checkbox" /><br />
+				<span class="description">Show a link at the end of each post
+				</span>			
+			</td>	
+		</tr>
+		<tr><th><label for="ljpl-content-author-text"></label></th>		
+			<td>
+				<input type="text" name="ljpl-content-author-text" id="ljpl-content-author-text" value="<?php echo esc_attr( get_the_author_meta( 'ljplcontentauthortext', $user->ID ) ); ?>" class="regular-text" /><br />
+				<span class="description">Add a text to the end of the post with
+				a link to your author page. ex. "Other post by".
+				</span>			
+			</td>
+			
+		</tr>
+
+	</table>
+<?php }
+
+add_action( 'personal_options_update', 'ljpl_gplus_link_field_save' );
+add_action( 'edit_user_profile_update', 'ljpl_gplus_link_field_save' );
+
+
+function ljpl_gplus_link_field_save( $user_id ) {
+
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+
+	update_usermeta( $user_id, 'gplus', $_POST['gplus'] );
+	update_usermeta( $user_id, 'ljpl-content-author-show', $_POST['ljpl-content-author-show'] );
+	update_usermeta( $user_id, 'ljpl-content-author-text', $_POST['ljpl-content-author-text'] );
+}
---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Przeprowadzka ===
+Contributors: ljasinskipl
+Donate link: http://www.ljasinski.pl
+Tags: google plus, gplus, author profile, rich snippet
+Requires at least: 2.0.2
+Tested up to: 3.2.1
+Stable tag: 1.0
+
+Simple plugin to get your author's profile linked in Google. 
+ Grants autorship photo in Google SERP. Visit blog.ljasinski.pl for more details.
+ All settings are per user, change them in your profile.
+== Description ==
+
+Simple plugin to get your author's profile linked in Google. 
+ Grants autorship photo in Google SERP. Visit blog.ljasinski.pl for more details.
+ All settings are per user, change them in your profile.
+ 
+Beware - plugin turns off default user's bio filtering. Don't use it, if you can't
+trust your users not to harm your website!
+== Installation ==
+
+Download and copy to plugins dir :)
+
+== Frequently Asked Questions ==
+
+= Where are all the settings? =
+
+In Your user's profile
+
+= I found a bug =
+
+Mail me at studio_wp@ljasinski.pl
+
+== Screenshots ==
+
+== Changelog ==
+
+= 1.0 =
+Stable version.
